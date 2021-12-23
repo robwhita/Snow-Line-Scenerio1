3f43efda70569239b4cc9ecc6b00594bced6e6c6
Rick Roll <robwhita@cisco.com>
2021-12-23 14:25:02 +0000

    broken script

--- oauth_demo.py
+++ oauth_demo.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-import requests
 import json
 
 
@@ -8,10 +7,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-clientID = "Cc603d54f07887692218aa80ce4c84f015442022968f3c2f68a2db65f604294c8"
-secretID = "b5acb66715c81feb4acb133a2fa02cca9db1aeb717ff49c91eb94eb2455d518b"
+clientID = "APP_CLIENTID"
+secretID = "APP_SECRETID"
 redirectURI = "http://0.0.0.0:10060/oauth" #This could be different if you publicly expose this endpoint.
-auth_url = f'https://webexapis.com/v1/authorize?client_id={clientID}&response_type=code&redirect_uri=http%3A%2F%2F0.0.0.0%3A10060%2Foauth&scope=spark%3Akms%20spark%3Apeople_read&state=set_state_here'
+auth_url = f'https://webexapis.com/v1/authorize?client_id=clientID&response_type=code&redirect_uri=http%3A%2F%2F0.0.0.0%3A10060%2Foauth&scope=spark%3Akms%20spark%3Apeople_read&state=set_state_here'
 
 def get_tokens(code):
     """Gets access token and refresh token"""
@@ -20,8 +19,8 @@ def get_tokens(code):
     headers = {'accept':'application/json','content-type':'application/x-www-form-urlencoded'}
     payload = ("grant_type=authorization_code&client_id={0}&client_secret={1}&"
                     "code={2}&redirect_uri={3}").format(clientID, secretID, code, redirectURI)
-    req = requests.post(url=url, data=payload, headers=headers)
-    results = json.loads(req.text)
+    req = requests.get(url=url, data=payload, headers=headers)
+    results = json.dumps(req.text)
     print(results)
     access_token = results["access_token"]
     refresh_token = results["refresh_token"]
@@ -43,7 +42,7 @@ def get_oauthuser_info(access_token):
 
 def main_page():
     """Main Grant page"""
-    return render_template("index.html", auth_url=auth_url)
+    return render_template("index.html",  auth_url=auth_url)
   
 
 @app.route("/oauth") #Endpoint acting as Redirect URI.
